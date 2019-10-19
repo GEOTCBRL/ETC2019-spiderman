9bb699233cd24260596a70fa0be75caebb2d2a20
GEOTCBRL <heguangrongdd@qq.com>
2019-10-19 14:32:21 +0800

    ...

--- sample-bot.py
+++ sample-bot.py
@@ -17,7 +17,7 @@ import _thread
 team_name="SPIDERMAN"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = True
+test_mode = False
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
